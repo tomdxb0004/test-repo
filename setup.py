1bad7edaefc8b90145ef83cd5c7d96ecea910d5d
TOM JOSEPH <tomdxb00004@gmail.com>
2020-09-10 17:06:12 +0400

    adding data pipeline

---
+++ setup.py
@@ -0,0 +1,12 @@
+setup(
+    ...
+    install_requires=[
+        ...
+        'click>=7.0'
+    ],
+	...
+    entry_points='''
+        [console_scripts]
+        titanic_analysis=titanic.command_line:titanic_analysis
+    '''
+)
